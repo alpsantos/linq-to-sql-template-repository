574767aac27b36760de59568a69ecd2cbb2dd23d
mozair.alves <mozair.alves@locaweb.com.br>
2010-02-19 14:09:19 -0200

    ooops

---
+++ ProjectFixture/MappingFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Codevil.TemplateRepository.Mappings;
+
+namespace ProjectFixture
+{
+    public class MappingFixture : Mapping<DataFixture, EntityFixture>
+    {
+        public override void UpdateRow(DataFixture row, EntityFixture entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateEntity(EntityFixture entity, DataFixture row)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EntityFixture {}
+
+    public class DataFixture {}
+}
